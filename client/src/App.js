348f1ba25de500ef43a8cbd8a69f253d4a4ae159
prashant9912 <prashantg9912@gmail.com>
2021-03-09 03:27:24 +0530

    basic client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Web3 from "web3";
+
+import ElectionContract from "./Election.json";
+
+export default class App extends Component {
+
+  componentDidMount=()=>{
+    this.loadBlockchainData()
+
+  }
+
+    state = { account: '',candidates:[] }
+
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
+    const accounts = await web3.eth.getAccounts()
+
+    
+    //contract instants
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = ElectionContract.networks[networkId];
+    const instance = new web3.eth.Contract(
+      ElectionContract.abi,
+      deployedNetwork && deployedNetwork.address,
+    );
+
+    const number = await instance.methods.candidatesCount().call();
+    console.log(number)
+    //
+
+      let candi=[]
+
+    for(let i=1;i<=number;i++){
+      await instance.methods.candidates(i).call().then(candidate=>{
+        let obb={}
+        obb.id=candidate.id
+        obb.name=candidate.name
+        obb.vote = candidate.voteCount
+        candi.push(obb)
+      })
+    }
+
+
+  
+    this.setState({ account: accounts[0],candidates:candi })
+
+
+  }
+
+
+  render() {
+    return <div>
+              <p>Your account: {this.state.account}</p>
+
+              {this.state.candidates.map((v,k)=><p key={k}>{v.name}</p>)}
+
+
+
+    </div>;
+  }
 }
 
-export default App;
+// App.contextType =  Theme
